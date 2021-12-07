206bc675fff7aa34f7816986f142974b3010d460
Hammad Qureshi <hammadahmadqureshi99@gmail.com>
2021-12-07 18:14:54 +0500

    Errors fixed in first signup page

--- resources/screens/Signup.js
+++ resources/screens/Signup.js
@@ -2,27 +2,29 @@ import React from 'react';
 import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import SignupScreenHeader from '../components/SignupScreenHeader';
 import Colors from '../colors/Colors';
+import Strings from '../strings/Strings';
+import UrduStrings from '../strings/UrduStrings';
 
 const Signup = props =>{
     return(
         <>
-        <SignupScreenHeader shiftLoginScreen={props.shiftLoginScreen} />  
+        <SignupScreenHeader shiftLoginScreen={props.shiftLoginScreen} setEnglish={props.setEnglish} title={props.setEnglish ? Strings.signupLabel : UrduStrings.signupLabel}/>  
         <View style={styles.container}>
             
             <View style={styles.sloganContainer} >
                 <Image source={require('../images/signupSlogan.png')} style={styles.sloganImage} />
-                <Text style={styles.slogan}>Health is a wealth</Text>
+                <Text style={styles.slogan}>{props.setEnglish ? Strings.signupQuote : UrduStrings.signupQuote}</Text>
             </View>
 
-            <Text style={{marginTop:10}}>We'll help you to create accounts, with easy steps.</Text>
-            
+            <Text style={{marginTop:10, textAlign:'center'}}>{props.setEnglish ? Strings.flowText : UrduStrings.flowText}</Text>
+
             <TouchableOpacity style={styles.button1} >
-                    <Text style={styles.btnText1}> Next </Text>
+                    <Text style={styles.btnText1}>{props.setEnglish ? Strings.nextText : UrduStrings.nextText}</Text>
             </TouchableOpacity>
         </View>
         <View style={{alignItems:'center'}}>
             <TouchableOpacity style={styles.button2} onPress={props.shiftLoginScreen}>
-                <Text style={styles.btnText2}> Already have an account? </Text>
+                <Text style={styles.btnText2}>{props.setEnglish ? Strings.loginText : UrduStrings.loginText}</Text>
             </TouchableOpacity>
         </View>
         </>
@@ -32,7 +34,8 @@ const Signup = props =>{
 const styles= StyleSheet.create({
     container:{
         flex:1,
-        padding: '10%'
+        padding: '10%',
+        alignItems:'center'
     },
     sloganContainer:{
         marginTop: 0,
