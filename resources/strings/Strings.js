d915565420ba4e0f4fd7f0f002a0c8cfca6f7ffe
oho_wajiha <wajihashiraz02@gmail.com>
2022-01-05 19:18:20 +0500

    Height/Weight Screen added

--- resources/strings/Strings.js
+++ resources/strings/Strings.js
@@ -1,48 +1,57 @@
-export default{
-    // Login Activity
+export default {
+  // Login Activity
 
-    //languageButtons
-    englishLanguage: "English",
-    urduLanguage: "Urdu",
+  //languageButtons
+  englishLanguage: 'English',
+  urduLanguage: 'Urdu',
 
-    // TextInputs
-    emailPlaceholder: "Email",
-    passwordPlaceholder: "Password",
-    // Buttons
-    loginButtonText: "Log In",
-    forgotPasswordText: "Forgot Password?",
-    // spliter
-    spliterText: "OR",
-    // signup Button
-    signupText: "CREATE NEW ACCOUNT",
+  // TextInputs
+  emailPlaceholder: 'Email',
+  passwordPlaceholder: 'Password',
+  // Buttons
+  loginButtonText: 'Log In',
+  forgotPasswordText: 'Forgot Password?',
+  // spliter
+  spliterText: 'OR',
+  // signup Button
+  signupText: 'CREATE NEW ACCOUNT',
 
+  //signup Avtivity
+  signupLabel: 'Choose User',
+  signupQuote: 'Welcome to "Fitness Center"',
+  flowText: 'Follow easy steps to create your account.',
+  chooseUserCaption: 'Choose the user type',
+  nextText: 'Next',
+  loginText: 'Already have an account?',
 
-    //signup Avtivity
-    signupLabel:"Choose User",
-    signupQuote: "Welcome to \"Fitness Center\"",
-    flowText: "Follow easy steps to create your account.",
-    chooseUserCaption: "Choose the user type",
-    nextText: "Next",
-    loginText: "Already have an account?",
+  nameLabelText: 'Name',
+  nameHeadingText: "What's you name?",
+  nameCaptionText: 'Enter your name',
+  firstNameLabel: 'First Name',
+  lastNameLabel: 'Last Name',
 
+  mailLabelText: 'Email',
+  mailHeadingText: 'Enter your email',
+  mailCaptionText: 'We wil notify you about updates through email.',
 
-    nameLabelText: "Name",
-    nameHeadingText:"What's you name?",
-    nameCaptionText:"Enter your name",
-    firstNameLabel: "First Name",
-    lastNameLabel: "Last Name",
+  passLabelText: 'Password',
+  passHeadingText: 'create password',
+  passCaptionText: 'Protect your account by making strong password.',
+  cPassLabelText: 'Confirm Password',
 
-    mailLabelText: "Email",
-    mailHeadingText:"Enter your email",
-    mailCaptionText:"We wil notify you about updates through email.",
-    
-    passLabelText: "Password",
-    passHeadingText:"create password",
-    passCaptionText:"Protect your account by making strong password.",
-    cPassLabelText: "Confirm Password",
+  dobLabelText: 'Birthday',
+  dobHeadingText: "What's your birthday",
+  dobCaptionText: 'Click on Calendar and choose your date of birth',
+  ageLabelText: ' Years old',
 
-    dobLabelText: "Birthday",
-    dobHeadingText: "What's your birthday",
-    dobCaptionText: "Click on Calendar and choose your date of birth",
-    ageLabelText: " Years old"
+  HeightLabelText: 'BODY MASS CALCULATION ',
+  HeightHeadingText: "What's your Height?",
+  HeightCaptionText: 'Enter your Height',
+  feetLabel: 'Feet',
+  inchesLabel: 'inches',
+  cmLabel: 'Enter Height (cm)',
+
+  WeightHeadingText: "What's your Weight?",
+  WeightCaptionText: 'Enter your Weight to calculate the BMI ',
+  kgLabel: 'Enter Weight (kg)',
 };
